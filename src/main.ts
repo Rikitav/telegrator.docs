612d98ff83ca03e521bce8b3611b4e47f9d0b30a
gutii <gutii@RIKITAV>
2026-04-29 02:48:03 +0400

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
-import {bootstrapApplication} from '@angular/platform-browser';
-import {App} from './app/app';
-import {appConfig} from './app/app.config';
+import {
+    BootstrapContext,
+    bootstrapApplication,
+} from '@angular/platform-browser';
+import { App } from './app/app';
+import { appConfig } from './app/app.config';
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
+
+const bootstrap = (context: BootstrapContext) =>
+    bootstrapApplication(App, appConfig, context);
+
+export default bootstrap;
